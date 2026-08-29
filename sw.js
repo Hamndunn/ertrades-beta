6c7e0c46e6e273bd2d92ccc3b71a8bca8e6db5f6
Hamndunn <ilhamphotograph1@gmail.com>
2026-08-29 14:45:00 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'ertrades-pwa-v82c';
+var CACHE_NAME = 'ertrades-pwa-v82f';
 self.addEventListener('install', function (event) { self.skipWaiting(); });
 self.addEventListener('activate', function (event) { event.waitUntil(caches.keys().then(function(keys){ return Promise.all(keys.map(function(key){ if(key.indexOf('ertrades-pwa-')===0 && key!==CACHE_NAME){ return caches.delete(key); } return Promise.resolve(); })); }).then(function(){ return self.clients.claim(); })); });
 self.addEventListener('fetch', function (event) { return; });
